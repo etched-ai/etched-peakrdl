10d973beac540ff23d8fb298183bcd1b00f93e04
daniyal0951 <daniyal@etched.com>
2026-01-04 21:06:52 -0800

    Simplify logging: use ENABLE_LOGGING instead of WAFER_SORT_SILENT/ENABLE_CSR_LOGGING
    - Update templates to use consistent ENABLE_LOGGING define
    - Add WS_LOG_INFO macro that respects ENABLE_LOGGING
    - Use csr_test_utils helper functions for GPIO reporting

--- src/etched_peakrdl_cheader/templates/rw_readwrite_test.c
+++ src/etched_peakrdl_cheader/templates/rw_readwrite_test.c
@@ -2,17 +2,51 @@
   curr_test_idx = (uint64_t)test_idx | (uint64_t){{test_idx}};
   if(!ignorer->ShouldSkipTestIndex(curr_test_idx)) {
 #ifdef ENABLE_WAFERSORT_GPIO
-    sival::wafersort::TestProgressGpio(curr_test_idx & 0xFF);
-    WS_LOG_INFO("Testing {{field}} (0x%llx)", (unsigned long long)curr_test_idx);
+{% if function_name == "BitFieldWriteReadTest256" %}
+#ifndef ENABLE_LOGGING
+    passed = sival::wafersort::csr::BitFieldWriteReadTest256WithReport(
+        {{reg_ptr}},
+        {{field_bp}},
+        {{field_bw}},
+        curr_test_idx,
+        nullptr);
+#else
+    passed = sival::wafersort::csr::BitFieldWriteReadTest256WithReport(
+        {{reg_ptr}},
+        {{field_bp}},
+        {{field_bw}},
+        curr_test_idx,
+        "{{field}}");
 #endif
+{% else %}
+#ifndef ENABLE_LOGGING
+    passed = sival::wafersort::csr::BitFieldWriteReadTest32WithReport(
+        {{reg_ptr}},
+        {{field_bp}},
+        {{field_bw}},
+        curr_test_idx,
+        nullptr);
+#else
+    passed = sival::wafersort::csr::BitFieldWriteReadTest32WithReport(
+        {{reg_ptr}},
+        {{field_bp}},
+        {{field_bw}},
+        curr_test_idx,
+        "{{field}}");
+#endif
+{% endif %}
+#else
+    WS_LOG_INFO("Testing {{field}} (0x%llx)",
+                (unsigned long long)curr_test_idx);
     passed = fw::testing::{{function_name}}({{reg_ptr}},
                         {{field_bp}},
                         {{field_bw}});
-#ifdef ENABLE_WAFERSORT_GPIO
     WS_LOG_INFO("  %s", passed ? "PASS" : "FAIL");
 #endif
   }
   if (!passed) {
+#ifndef ENABLE_WAFERSORT_GPIO
     fw::testing::TestFail((uint64_t)0xDEAD000000000000 | curr_test_idx);
+#endif
     return false;
   }
--- src/etched_peakrdl_cheader/templates/rw_test_registers_header.c
+++ src/etched_peakrdl_cheader/templates/rw_test_registers_header.c
@@ -5,7 +5,17 @@
 {% if hasRegOrRegFile %}#include "fw/testing/bit_field_test.h"
 #include "fw/testing/testing.h"
 #include "fw/utils/csr_descriptor_helper.h"
+#include "fw/utils/log.h"
+#ifndef WS_LOG_INFO
+#ifndef ENABLE_LOGGING
+#define WS_LOG_INFO(...) \
+  do {                   \
+  } while (0)
+#else
+#define WS_LOG_INFO(...) LOG_INFO_BLOCKING(__VA_ARGS__)
+#endif
+#endif
 #ifdef ENABLE_WAFERSORT_GPIO
-#include "sival/wafersort/sival_helper.h"
+#include "sival/wafersort/csr/csr_test_utils.h"
 #endif
 {% endif %}
